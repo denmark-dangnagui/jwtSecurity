cebbf898057f7aa1e34daedeb0846d3f0a34ea36
Taemin Lee <dlrgks88@naver.com>
2024-03-19 13:02:57 +0900

    회원가입 로직 생성

---
+++ SpringJWT/src/main/java/com/example/springjwt/service/JoinService.java
@@ -0,0 +1,38 @@
+package com.example.springjwt.service;
+
+import com.example.springjwt.dto.JoinDTO;
+import com.example.springjwt.entity.UserEntity;
+import com.example.springjwt.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JoinService {
+
+    private final UserRepository userRepository;
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public JoinService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public void joinProcess(JoinDTO joinDTO) {
+        String username = joinDTO.getUsername();
+        String password = joinDTO.getPassword();
+
+        Boolean isExist = userRepository.existsByUsername(username);
+
+        if (isExist) {
+
+        }
+        UserEntity newUser = new UserEntity();
+
+        newUser.setUsername(username);
+        newUser.setPassword(bCryptPasswordEncoder.encode(password));
+        newUser.setRole("ROLE_ADMIN");
+
+        userRepository.save(newUser);
+    }
+}
